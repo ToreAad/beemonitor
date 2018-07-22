5515547c324ef3b47fa872b443a6af74d8f3b773
Tore Aadland <tore.aad@gmail.com>
2018-07-22 22:55:21 +0200

    Adapted the files from uploading camera data to uploading temperature and humidity data.

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/pi/beemonitor/
+FILEDATE=$(date + "%Y-%m-%d")
+DATADATE=$(date + "%Y-%m-%d_%H%M")
 DATA=$(am2301)
-python upload_data.py DATA
+echo DATADATE DATA >> DATA
+#python upload_data.py DATA
--- upload_data.py
+++ upload_data.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
-
 import dropbox
 import sys
+
 import json
+
+with open ("auth.secret") as f:
+    auth_data = json.load(f)
+
+access_token = auth_data["access_token"]
 
 
 class TransferData:
@@ -13,11 +18,16 @@ class TransferData:
         dbx = dropbox.Dropbox(self.access_token)
 
         with open(file_from, 'rb') as f:
-            dbx.files_upload(f.read(), file_to)
+            dbx.files_upload(f.read(), file_to, mode=WriteMode('overwrite', None))
 
 
 def main():
-    pass
+    transferData = TransferData(access_token)
+
+    file_from = './'+sys.argv[1]
+    file_to = '/'+sys.argv[1]
+
+    transferData.upload_file(file_from, file_to )
 
 if __name__ == '__main__':
     main()
